1b4f155122cf09d5afa6a048c4701c599411a7d9
Rob Grimes <grimesrob8@gmail.com>
2020-05-25 09:20:10 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express 				= require("express"), 
+	app 	 				= express(), 
+	bodyParser				= require("body-parser"), 
+	mongoose 				= require("mongoose"),
+	flash					= require("connect-flash"),
+	passport				= require("passport"),
+	LocalStrategy 			= require("passport-local"),
+	methodOverride			= require("method-override"),
+	passportLocalMongoose 	= require("passport-local-mongoose");
+
+//mongoose.connect("mongodb://localhost:27017/hummingbird", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(flash());
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.listen(3000, function(){
+	console.log("Server is running");
+})
---
+++ public/scripts/myjquery.js
@@ -0,0 +1,3 @@
+$('.parallax-window').parallax({imageSrc: '/images/coffee2.jpg'});
+
+$('.parallax-window-2').parallax({imageSrc: '/images/coffee3.jpg'});
