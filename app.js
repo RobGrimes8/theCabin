88b50e59c6c7972aa718b8cf5c57ef61cf03ba50
robgrimes8 <grimesrob8@gmail.com>
2020-05-28 17:18:11 +0000

    revert back to nodemailer

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var express 				= require("express"),
 	methodOverride			= require("method-override"),
 	passportLocalMongoose 	= require("passport-local-mongoose"),
 	nodemailer 				= require("nodemailer"),
-	mg = require('nodemailer-mailgun-transport'),
-	Message 		= require("./models/message");
+	mg = require('nodemailer-mailgun-transport');
+	// Message 		= require("./models/message");
 
-mongoose.connect("mongodb://localhost:27017/hummingbird", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+// mongoose.connect("mongodb://localhost:27017/hummingbird", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
@@ -30,43 +30,43 @@ app.post("/about", function(req, res){
 	
 //========= Message page method. ============//
 	
-//Get data from form and push to campgrounds array
-	var newMessage = req.body.message;
-	//create new campground and save to database
-	Message.create(newMessage, function(err, message){
- 		if(err){
- 			console.log(err);
- 			console.log("Error occured.")
- 		}else{
-			console.log(message);
- 			res.redirect("/");
- 		}
- 	});
+// //Get data from form and push to campgrounds array
+// 	var newMessage = req.body.message;
+// 	//create new campground and save to database
+// 	Message.create(newMessage, function(err, message){
+//  		if(err){
+//  			console.log(err);
+//  			console.log("Error occured.")
+//  		}else{
+// 			console.log(message);
+//  			res.redirect("/");
+//  		}
+//  	});
 	
 //========= Email sender method. ============//	
-	// // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
-	// var auth = {
-	//   auth: {
-	// 	api_key: 'e2aeeef39623a26de1bdaa362124b2b5-7fba8a4e-37cff87b',
-	// 	domain: 'sandbox9c133f797e2d43539301482c178e53b7.mailgun.org'
-	//   }
-	// }
+	// This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+	var auth = {
+	  auth: {
+		api_key: 'e2aeeef39623a26de1bdaa362124b2b5-7fba8a4e-37cff87b',
+		domain: 'sandbox9c133f797e2d43539301482c178e53b7.mailgun.org'
+	  }
+	}
 
-	// var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+	var nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
-	// nodemailerMailgun.sendMail({
-	//   from: req.body.email,
-	//   to: 'grimesrob8@gmail.com', // An array if you have multiple recipients.
-	//   subject: req.body.subject,
-	//   text: req.body.message,
-	// }, function (err, info) {
-	//   if (err) {
-	// 	console.log('Error: ' + err);
-	//   }
-	//   else {
-	// 	res.render("landing");
-	//   }
-	// });
+	nodemailerMailgun.sendMail({
+	  from: req.body.email,
+	  to: 'grimesrob8@gmail.com', // An array if you have multiple recipients.
+	  subject: req.body.subject,
+	  text: req.body.message,
+	}, function (err, info) {
+	  if (err) {
+		console.log('Error: ' + err);
+	  }
+	  else {
+		res.render("landing");
+	  }
+	});
 	
 });
 
