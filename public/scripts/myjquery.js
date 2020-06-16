52cd0377bdbbc417f1399f6f892f5eabe3aee66b
Rob <grimesrob8@gmail.com>
2020-06-16 15:50:38 +0100

    initial landing page layout

--- public/scripts/myjquery.js
+++ public/scripts/myjquery.js
@@ -5,3 +5,10 @@ $('.parallax-window-2').parallax({ imageSrc: '/images/coffee3-min.jpg' });
 $('.parallax-window-3').parallax({ imageSrc: '/images/coffeeMap.jpg' });
 
 $('.parallax-window-4').parallax({ imageSrc: '/images/coffeeParalax4.jpg' });
+
+$("#aboutLink").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".about").offset().top - 90
+        },
+        'slow');
+});
