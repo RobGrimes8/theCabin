ee402440e4239e99e48c0f477fc8e3f6917f1399
Rob <grimesrob8@gmail.com>
2020-06-21 10:56:13 +0100

    comments in newest first and images moved to db

--- app.js
+++ app.js
@@ -20,15 +20,19 @@ const commentSchema = new mongoose.Schema({
 });
 const Comment = mongoose.model("Comment", commentSchema);
 
+const imageSchema = new mongoose.Schema({
+    image: String,
+    title: String,
+    desc: String
+});
+const Image = mongoose.model("Image", imageSchema);
+
 mongoose.connect('mongodb+srv://' + process.env._MONGOOSE + '@cluster0-rkzfs.mongodb.net/The_Cabin?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     // we're connected!
     console.log("Db connected")
-
-
-
 
     // const silence = new Comment({ name: "Beth", message: "I love cake!" });
     // silence.save(function(err, silence) {
@@ -52,60 +56,19 @@ app.get("/", function(req, res) {
 });
 
 app.get("/gallery", function(req, res) {
+    var imageArr = [];
+    var commentsArr = [];
 
-    var allPhotos = [{
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/s1080x2048/103364015_950228958765182_5072206665611311081_o.jpg?_nc_cat=111&_nc_sid=9267fe&_nc_ohc=X8qP2TSbGO8AX_AKO6S&_nc_ht=scontent-lhr8-1.xx&_nc_tp=7&oh=306189f1848259a30455f7b150b462c0&oe=5F117D7A",
-            title: "Birthday cards!",
-            desc: "Birthday coming up? Best friend got a new job? We have cards from Choo Choo at our counter for you to have a look at. £2.50 each!✨",
-            photoId: "1"
-        },
-        {
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/s1080x2048/104459363_950105438777534_7362702774212807111_o.jpg?_nc_cat=107&_nc_sid=9267fe&_nc_ohc=mT5pQv5OESkAX9GC453&_nc_ht=scontent-lhr8-1.xx&_nc_tp=7&oh=1b23764b028b2f1a720db6a0d8f6ca23&oe=5F0F60B4",
-            title: "Syrups!",
-            desc: "New syrups have arrived! Some sugar free too, come and give them a try!",
-            photoId: "2"
-        },
-        {
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/s1080x2048/101791813_945915785863166_6621990462267366921_o.jpg?_nc_cat=110&_nc_sid=9267fe&_nc_ohc=2QPt8irOxkYAX9XINV_&_nc_ht=scontent-lhr8-1.xx&_nc_tp=7&oh=45d5ab8a97293bf46f7cfccc3c67f023&oe=5F0F2761",
-            title: "Food was brilliant",
-            desc: "Another gorgeous day in Ruthin! Second window finished. Time for a coffee!🌞🌈",
-            photoId: "3"
-        },
-        {
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/100105873_933786740409404_9185360274910085120_o.jpg?_nc_cat=107&_nc_sid=9267fe&_nc_ohc=sl8FTHQYmrUAX9g8rKJ&_nc_ht=scontent-lhr8-1.xx&oh=66f42358542a62a0cabbe25b6b991710&oe=5F12BB70",
-            title: "Amazing weather today!",
-            desc: "Granted, the weather is terrible today. But we have fresh hot coffee and delicious cakes for you to take home and enjoy, why not pop in and treat yourself? #localbusiness",
-            photoId: "4"
-        },
-        {
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/103287452_944450469343031_6369883692757897880_o.jpg?_nc_cat=110&_nc_sid=9267fe&_nc_ohc=NBsz_tr-Cr0AX-qY0if&_nc_ht=scontent-lhr8-1.xx&oh=2238c5f728ba8ea7fc738e521729e0b1&oe=5F0FA93D",
-            title: "Food was brilliant",
-            desc: "Another gorgeous day in Ruthin! Second window finished. Time for a coffee!🌞🌈",
-            photoId: "5"
-        },
-        {
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/82518908_938809193240492_6423717413346344960_o.jpg?_nc_cat=109&_nc_sid=a26aad&_nc_ohc=Oh46nVufresAX-IsuLf&_nc_ht=scontent-lhr8-1.xx&oh=9d78b78ec730affcf0e2f9f29189784b&oe=5F11BB7A",
-            title: "Todays cup of coffee!",
-            desc: "Granted, the weather is terrible today. But we have fresh hot coffee and delicious cakes for you to take home and enjoy, why not pop in and treat yourself? #localbusiness",
-            photoId: "6"
-        },
-        {
-            image: "https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/88253696_876165216171557_742865433371082752_o.jpg?_nc_cat=100&_nc_sid=9267fe&_nc_ohc=DuvV6WdDmjwAX_CFB62&_nc_ht=scontent-lht6-1.xx&oh=e91547906b5a3123bd45bcbb8053deff&oe=5F11389E",
-            title: "Food was brilliant",
-            desc: "Another gorgeous day in Ruthin! Second window finished. Time for a coffee!🌞🌈",
-            photoId: "7"
-        },
-        {
-            image: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/82794666_846750642446348_7563659870382063616_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_ohc=jRlLDkeOUAUAX_KvAwD&_nc_ht=scontent-lhr8-1.xx&oh=d7e1c25879d7a7f177729784dbc7a7f6&oe=5F114E1D",
-            title: "Todays cup of coffee!",
-            desc: "Granted, the weather is terrible today. But we have fresh hot coffee and delicious cakes for you to take home and enjoy, why not pop in and treat yourself? #localbusiness",
-            photoId: "8"
-        }
-    ];
+    Image.find(function(err, images) {
+        if (err) return console.error(err);
+        imageArr = images;
+    });
 
     Comment.find(function(err, comments) {
         if (err) return console.error(err);
-        res.render("gallery", { photos: allPhotos, comments: comments });
+        let reverseComments = comments.reverse();
+        commentsArr = reverseComments;
+        res.render("gallery", { photos: imageArr, comments: commentsArr });
     });
 
 
